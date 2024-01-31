a153048cdb1dfd8937d642a0dcdcaee6a2ec9370
Lee JaeJun <monooi@naver.com>
2024-01-31 14:01:11 +0900

    Init: Initial project settings

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: "/popup.html",
+        background: "/src/background.ts",
+        contentScript: "/src/contentScript.ts",
+      },
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+});
